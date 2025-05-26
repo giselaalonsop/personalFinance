e03d5e1428664d3d7a514746bdc4e19a96633620
giselaalonsop <giselaalonsop@gmail.com>
2025-05-26 18:01:36 -0400

    variable

--- src/main/java/com/gcode/personalFinance/PersonalFinanceApplication.java
+++ src/main/java/com/gcode/personalFinance/PersonalFinanceApplication.java
@@ -3,6 +3,10 @@ package com.gcode.personalFinance;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PersonalFinanceApplication {
@@ -11,5 +15,18 @@ public class PersonalFinanceApplication {
 		SpringApplication.run(PersonalFinanceApplication.class, args);
 		log.info("Servicio Iniciado");
 	}
+	@Configuration
+	public static class MyConfiguration {
 
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
