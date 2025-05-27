0baac5b17e2403278a3e9a18d029d5aa98f87914
giselaalonsop <giselaalonsop@gmail.com>
2025-05-27 13:06:43 -0400

    variable

--- src/main/java/com/gcode/personalFinance/config/SecurityConfig.java
+++ src/main/java/com/gcode/personalFinance/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -29,6 +33,16 @@ public class SecurityConfig {
     @PostConstruct
     public void logLoaded() {
         System.out.println("✅ SecurityConfig CARGADO");
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
