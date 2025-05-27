224d5bc080f97a48b3743b7ad550b4cdbd61e8cb
giselaalonsop <giselaalonsop@gmail.com>
2025-05-27 13:09:26 -0400

    variable

--- src/main/java/com/gcode/personalFinance/PersonalFinanceApplication.java
+++ src/main/java/com/gcode/personalFinance/PersonalFinanceApplication.java
@@ -3,12 +3,14 @@ package com.gcode.personalFinance;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class PersonalFinanceApplication {
 	private static Logger log = org.apache.logging.log4j.LogManager.getLogger(PersonalFinanceApplication.class);
 	public static void main(String[] args) {
